892c990ed741aeae40abccf4bc19cf6fb09a77a0
0l3x <48629513+0l3x@users.noreply.github.com>
2025-05-07 14:18:24 +0200

    chagpt malvado

--- PhysioCareOlex/auth/auth.js
+++ PhysioCareOlex/auth/auth.js
@@ -7,7 +7,7 @@ dotenv.config();
 // Función para generar un token
 const generarToken = (usuario) => {
     return jwt.sign(
-        { login: usuario.login, rol: usuario.rol, id: idActual },
+        { login: usuario.login, rol: usuario.rol },
         process.env.SECRETO,
         { expiresIn: '1h' }
     );
